4e8da3cb4b620c1005c4d84e7ad2af77ec02c886
plaerke <plaerke@me.com>
2022-04-25 11:00:58 +0200

    Prettified the js

--- src/script.js
+++ src/script.js
@@ -1,19 +1,19 @@
 function formatDate(timestamp) {
-let date = new Date(timestamp);
-let hours = date.getHours();
-if (hours < 10) {hours = `0${hours}`};
-let minutes = date.getMinutes();
-if (minutes < 10) {minutes = `0${minutes}`};
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[date.getDay()];
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {hours = `0${hours}`};
+  let minutes = date.getMinutes();
+  if (minutes < 10) {minutes = `0${minutes}`};
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   return `Last updated on ${day} ${hours}:${minutes}`;
 }
 
